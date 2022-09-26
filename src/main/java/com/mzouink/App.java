e8cc949f0a9835adcbe3af45b98bff110877d087
mzouink <zouinkhi.marwan@gmail.com>
2022-09-26 16:07:33 -0400

    initial

---
+++ src/main/java/com/mzouink/App.java
@@ -0,0 +1,13 @@
+package com.mzouink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
